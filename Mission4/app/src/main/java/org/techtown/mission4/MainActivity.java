46ac2fdba3abc550bc51c653f2085b4e6647ebec
eunchan <eunchan0492@naver.com>
2020-04-08 16:22:21 +0900

    2020.04.08

---
+++ Mission4/app/src/main/java/org/techtown/mission4/MainActivity.java
@@ -0,0 +1,50 @@
+package org.techtown.mission4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView checkByte;
+    Button okButton;
+    Button closeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+        checkByte = (TextView)findViewById(R.id.checkByte);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkByte.setText(editable.length() + " / 80 바이트");
+            }
+        });
+    }
+
+    public void onButtonClicked(View v){
+            Toast.makeText(this, editText.getText().toString(), Toast.LENGTH_LONG).show();
+    }
+}
---
+++ Mission6/app/src/main/java/org/techtown/mission6/MainActivity.java
@@ -0,0 +1,40 @@
+package org.techtown.mission6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView textView;
+    ProgressBar pgBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        textView = (TextView)findViewById(R.id.textView);
+        pgBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText(String.valueOf(i));
+                pgBar.setProgress(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
